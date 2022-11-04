47be5b17c74e545cc1abdbd48d1180e4d20b7ce6
Muhammad_Danish <danishpk175@gmail.com>
2022-11-04 15:51:12 +0530

    first commit

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,12 @@
+const ScoreBoard = ({ score }) => {
+  return (
+    <div className="score-board" >
+      <h1>ScoreBoard</h1>
+      <h2>{score}</h2>
+      <h5>&copy;Muhammad Danish</h5>
+
+    </div >
+  )
+}
+
+export default ScoreBoard
